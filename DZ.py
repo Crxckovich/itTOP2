f5a896e72b1a6d2a957ea1dcc20bd4a5e8740321
ZhopaBobra1337 <artpeshov281402@gmail.com>
2023-10-15 18:42:00 +0300

    Create DZ.py

---
+++ DZ.py
@@ -0,0 +1,47 @@
+# 1 ЫВЫГАНЕ
+
+# num = 2323
+# digit = int(str(num)[0])
+# print(digit)
+
+# 2 ЗЫДАНЕГЕ
+
+# a = 4
+# b = 7
+# c = 4
+
+# if a == b or a == c or b == c:
+#     print('YES')
+# else:
+#     print('NO')
+
+# 3 ЗЫДАВОНЕ
+
+# A = 5
+# B = 10
+
+# if A < B:
+#     for i in range(A, B+1):
+#         print(i)
+# else:
+#     for i in range(A, B-1, -1):
+#         print(i)
+
+# 4 ЫВАДАНИИ
+
+# str = 'а чо а ничо а чо ну типа лол ну лол ну типа'
+
+# count = 1
+
+# for i in str:
+#     if i == ' ':
+#         count += 1
+
+# print('Кол-во:', count)
+
+# 5 ОВАЗДЫНЕ
+
+# a = [1, 7, 4, 3]
+# for number in a:
+#     if number % 2 == 0:
+#         print(number)
